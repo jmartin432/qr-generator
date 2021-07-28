2f085f2ff26e5f8a47e9634c7f19bb5a6e751f9a
Justin <info@justinlmartin.com>
2021-07-28 01:28:29 -0700

    Working QR code in bucket, have to fix lambda response

--- lambdas/qr-generator-lambda/qr-generator-lambda.py
+++ lambdas/qr-generator-lambda/qr-generator-lambda.py
@@ -15,13 +15,18 @@ qr_code_bucket_url = os.environ['QRCodeBucketUrl']
 def handler(event, context):
     logger.info(event)
     request_body = json.loads(event['body'])
-    file_name = Template('$name.$extension').safe_substitute(name=request_body['qr_code_name'], extension=request_body['file_type'])
+    file_name = Template('$name.$extension').safe_substitute(name=request_body['qr-code-name'],
+                                                             extension=request_body['file-type'])
 
-    qr_code = segno.make(request_body['data'])
+    temp_file = Template('/tmp/$name').safe_substitute(name=file_name)
+
+    qr_code = segno.make(request_body['data'], micro=request_body['micro'])
+    qr_code.save(temp_file)
+
     url = Template('$base/$path').safe_substitute(base=qr_code_bucket_url, path=file_name)
 
     s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(qr_code, qr_code_bucket, file_name)
+    s3.meta.client.upload_file(temp_file, qr_code_bucket, file_name)
 
     response = {
         'statusCode': 200,
