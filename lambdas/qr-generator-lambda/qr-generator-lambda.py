8beb278ceedace18ea319944d774ce0907689dc5
Justin <info@justinlmartin.com>
2021-07-29 17:33:27 -0700

    added border and color parameters for qr maker

--- lambdas/qr-generator-lambda/qr-generator-lambda.py
+++ lambdas/qr-generator-lambda/qr-generator-lambda.py
@@ -4,6 +4,7 @@ import logging
 import os
 import boto3
 from string import Template
+from urllib import parse
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -15,32 +16,43 @@ def handler(event, context):
     logger.info(event)
     stage = event['requestContext']['stage']
     request_body = json.loads(event['body'])
-    file_name = Template('$name.$extension').safe_substitute(name=request_body['qr-code-name'],
-                                                             extension=request_body['file-type'])
-    file_path = Template('$stage/$name').safe_substitute(stage=stage, name=file_name)
+    qr_code_name = 'new-code' if 'qr-code-name' not in request_body else request_body['qr-code-name']
+    file_type = 'png' if 'file-type' not in request_body else request_body['file-type']
+    data = 'www.justinlmartin.com' if 'data' not in request_body else request_body['data']
+    scale = 1 if 'scale' not in request_body else request_body['scale']
+    micro = False if 'micro' not in request_body else request_body['micro']
+    dark = 'black' if 'dark' not in request_body else request_body['dark']
+    light = 'white' if 'light' not in request_body else request_body['light']
+    border = 4 if 'border' not in request_body else request_body['border']
+    file_name = Template('$name.$extension').safe_substitute(name=qr_code_name, extension=file_type)
+    file_key = Template('$stage/$name').safe_substitute(stage=stage, name=file_name)
     temp_file = Template('/tmp/$name').safe_substitute(name=file_name)
-    content_type = Template('image/$type').safe_substitute(type=request_body['file-type'])
-
-    qr_code = segno.make(request_body['data'], micro=request_body['micro'])
-    qr_code.save(temp_file, scale=request_body['scale'])
-
+    content_type = Template('image/$type').safe_substitute(type=file_type)
     url = Template('https://$bucketName.s3.amazonaws.com/$path').safe_substitute(bucketName=qr_code_bucket,
-                                                                                 path=file_path)
+                                                                                 path=file_key)
+
+    qr_code = segno.make(data, micro=micro)
+    qr_code.save(temp_file, scale=scale, dark=dark, light=light, border=border)
+
+    tags = {
+        "public": "yes"
+    }
+
+    extra_args = {
+        "ContentType": content_type,
+        "Metadata": {
+            "data": data,
+            "scale": str(scale),
+            "micro": str(micro),
+            "dark": dark,
+            "light": light,
+            "border": str(border)
+        },
+        "Tagging": parse.urlencode(tags)
+    }
 
     s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(temp_file, qr_code_bucket, file_path, ExtraArgs={"ContentType": content_type})
-    s3.meta.client.put_object_tagging(
-        Bucket=qr_code_bucket,
-        Key=file_path,
-        Tagging={
-            'TagSet': [
-                {
-                    'Key': 'public',
-                    'Value': 'yes'
-                },
-            ]
-        }
-    )
+    s3.meta.client.upload_file(temp_file, qr_code_bucket, file_key, ExtraArgs=extra_args)
 
     response_body = {
         "url": url
